c35fd99aaa835b7aacf86dea9277396856f0811c
Rejwan-Sharif <mohammedrejwan8@gmail.com>
2023-09-01 22:22:16 +0600

    removed unnecessery code

--- src/main/java/com/operation/newspaper/NewspaperDataAccess.java
+++ src/main/java/com/operation/newspaper/NewspaperDataAccess.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.operation.db;
